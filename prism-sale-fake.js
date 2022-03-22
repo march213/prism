c163dd3d296e9dc0217026cd5b0be9978e12ad44
march213 <jane.molodetskaya@gmail.com>
2022-03-21 22:50:30 -0400

    inial commit

---
+++ prism-sale-fake.js
@@ -0,0 +1,26 @@
+class PrismSale {
+  constructor () {
+    this.totalSales = 0
+    this.maxSales = 100
+    this.owner = "0x..."
+    this.charity = "0x..."
+    this.sales = {
+    }
+  }
+
+  canBuy () {
+    return this.totalSales < this.maxSales
+  }
+
+  hasAccess () {
+    //////
+    return this.sales[address]
+  }
+
+  buy() {
+    ///
+    this.sales[address] = true
+  }
+}
+
+
