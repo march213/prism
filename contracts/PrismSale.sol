77d19a6e0e41b35d9451b237747b27fa050648a0
march213 <jane.molodetskaya@gmail.com>
2022-04-02 22:41:55 -0400

    update PrismSale contract -> add some functionality; add migration file

--- contracts/PrismSale.sol
+++ contracts/PrismSale.sol
@@ -2,6 +2,30 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract PrismSale {
-  constructor() public {
-  }
+    uint256 public totalSales;
+    uint256 public maxSales;
+
+    address public owner;
+    address public charity;
+
+    constructor() {
+        totalSales = 0;
+        maxSales = 100;
+
+        owner = 0x0702CBd5F006391CEFF3Dd6B22665ef3b4bBE826;
+        charity = 0x4020260cAb475dC05Cd0772992a4022C59deFEF2;
+    }
+
+    function canBuy() public view returns (bool) {
+        return totalSales < maxSales;
+    }
+
+    function buy() public returns (bool) {
+        if (canBuy()) {
+            totalSales++;
+            return true;
+        }
+
+        return false;
+    }
 }
